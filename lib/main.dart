51182bcf83bbf9c1457caf15c6fe2ed960c2f3a7
alejorom <morojela-github@yahoo.com>
2022-03-17 15:10:27 -0500

    Estructura inicial app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mi_uber_clone/pages/pages.dart';
+
+void main() {
+  runApp(const UberClone());
+}
+
+class UberClone extends StatefulWidget {
+  const UberClone({Key? key}) : super(key: key);
+
+  @override
+  State<UberClone> createState() => _UberCloneState();
+}
+
+class _UberCloneState extends State<UberClone> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'UberClone',
+      initialRoute: 'home',
+      routes: {
+        'home': (BuildContext context) => const HomePage(),
+      },
+    );
+  }
+}
